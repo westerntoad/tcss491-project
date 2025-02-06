a4aa4ef74490b2690fde610cefc2bad5fcaafb10
khammett325995 <khammett325995@gmail.com>
2025-02-06 13:36:24 -0800

    Delete src/entities/boss.js

--- src/entities/boss.js
+++
@@ -1,7 +0,0 @@
-class Boss extends Enemy {
-    constructor(name, hp, attack, defense, speed) {
-        super(name, hp, attack, defense, speed);
-        this.isBoss = true;
-    }
-    // add boss mechanics/characteristic
-}
